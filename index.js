9b11e3f665479a9924d257d928c0a2dfbd113a36
levin790 <44037871+levin790@users.noreply.github.com>
2025-01-20 04:35:51 +0800

    随机生成并替换了userID.

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ import { connect } from 'cloudflare:sockets';
  * User configuration and settings
  * Generate UUID: [Windows] Press "Win + R", input cmd and run: Powershell -NoExit -Command "[guid]::NewGuid()"
  */
-let userID = 'd342d11e-d424-4583-b36e-524ab1f0afa4';
+let userID = '7b31dfc3-50d7-452b-b5a3-8c356342166c';
 
 /**
  * Array of proxy server addresses with ports
